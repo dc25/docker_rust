91cc3f980b8f8473361826118d9b0ed3ab1d4e56
Dave Compton <davecompton7@gmail.com>
2019-01-30 21:38:57 -0800

    reverting to previous nix.  installing a few libraries. installing cachix

--- build_scripts/install_nix.sh
+++ build_scripts/install_nix.sh
@@ -1,8 +1,14 @@
 #! /bin/bash 
 
 echo "installing nix build system"
-curl https://nixos.org/nix/install | sh
+## curl curl https://nixos.org/nix/install | sh
+curl https://nixos.org/releases/nix/nix-2.1.3/install | sh
 
 . ~/.nix-profile/etc/profile.d/nix.sh
 
-nix-env -i cabal2nix
+nix-env -f "<nixpkgs>" -iA haskellPackages.aeson
+nix-env -f "<nixpkgs>" -iA haskellPackages.HTTP
+nix-env -f "<nixpkgs>" -iA haskellPackages.ghcid
+
+## per https://hie-nix.cachix.org/
+nix-env -f https://cachix.org/api/v1/install -iA cachix 
