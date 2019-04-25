490bcaa5fceab6154a73cb5dce31480fc2220d98
Dave Compton <davecompton7@gmail.com>
2019-04-25 11:49:32 -0700

    adding rust-fmt and clippy.

--- build_scripts/install_rust_helpers.sh
+++ build_scripts/install_rust_helpers.sh
@@ -7,3 +7,6 @@ source ~/.cargo/env
 ## per https://stackoverflow.com/questions/37706999/error-installing-a-crate-via-cargo-specified-package-has-no-binaries
 cargo install cargo-edit
 
+rustup component add clippy
+rustup component add rustfmt
+
