a1f258e3ef0238d4184b8d29718eec24974973dd
Dave Compton <davecompton7@gmail.com>
2019-11-17 13:17:18 -0800

    adding curl command for neovim

--- build_scripts/setup_vim_plug.sh
+++ build_scripts/setup_vim_plug.sh
@@ -2,17 +2,11 @@
 
 
 
-sudo apt-get update 
-
-sudo apt-get install -y \
-    git 
-
-
 # install vim-plug per https://github.com/junegunn/vim-plug
 
-# #for nvim
-# curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-#         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#for nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
