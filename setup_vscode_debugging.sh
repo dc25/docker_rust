9bea3a7720a6ac57590a7964ee7ce233e954f4d1
Dave Compton <davecompton7@gmail.com>
2020-09-14 21:45:03 -0700

    installing some libraries that are now required.

--- setup_vscode_debugging.sh
+++ setup_vscode_debugging.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y libxcb-dri3-dev
+sudo apt install -y libdrm-dev
+sudo apt install -y libgbm-dev
+
 export VSC_EXTENSIONS='rust-lang.rust vadimcn.vscode-lldb'
 # export VSC_EXTENSIONS='vadimcn.vscode-lldb'
 
