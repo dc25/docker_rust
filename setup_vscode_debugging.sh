d81694fc7c5c790097f93d2d0f71134dfd978cee
Dave Compton <davecompton7@gmail.com>
2021-01-09 18:09:16 -0800

    switching to rust-analyzer instead of rls for both vscode and vim

--- setup_vscode_debugging.sh
+++ setup_vscode_debugging.sh
@@ -8,8 +8,8 @@ sudo apt install -y libxcb-dri3-dev
 sudo apt install -y libdrm-dev
 sudo apt install -y libgbm-dev
 
-export VSC_EXTENSIONS='rust-lang.rust vadimcn.vscode-lldb'
-# export VSC_EXTENSIONS='vadimcn.vscode-lldb'
+# export VSC_EXTENSIONS='rust-lang.rust vadimcn.vscode-lldb'
+export VSC_EXTENSIONS='vadimcn.vscode-lldb'
 
 for s in $VSC_EXTENSIONS; do
     code --install-extension $s
