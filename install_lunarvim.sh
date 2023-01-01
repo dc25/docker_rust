ee015a9f8ba0c1cfda001c3ed6130542298248b5
dave <davecompton7@gmail.com>
2023-01-01 08:40:31 -0800

    added install_lunarvim

--- install_lunarvim.sh
+++ install_lunarvim.sh
@@ -3,7 +3,9 @@
 source ~/.cargo/env
 
 # pip, cargo, npm all needed for lunarvim; cargo & npm should already be installed.
-# sudo apt-get install  -y python3 python3-pip
-cd /tmp
+sudo apt-get install  -y python3 python3-pip
 
-yes | bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+INSTALL=/tmp/install.sh
+curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh > $INSTALL
+chmod +x $INSTALL
+$INSTALL -y
